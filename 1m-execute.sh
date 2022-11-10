c025edcfec9e4d97390439716df79477865921f4
abraxas678 <abraxas678@gmail.com>
2022-11-10 08:23:02 +0100

    Update 1m-execute.sh

--- 1m-execute.sh
+++ 1m-execute.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
-mkdir /home/abraxas/myq/
 cd /home/abraxas/myq/service
-mkdir /home/abraxas/myq/service
-[[ ! -f /home/abraxas/myq/service/1m.sh ]] && wget https://raw.githubusercontent.com/abraxas678/start6/master/1m.sh
-cd /home/abraxas/myq
-[[ ! -f /etc/systemd/system/1m.timer ]] && wget https://raw.githubusercontent.com/abraxas678/start6/master/1m.timer
-if [[ ! -f /etc/systemd/system/1m.service ]];then
-    wget https://raw.githubusercontent.com/abraxas678/start6/master/1m.service
-   # echo "ExecStart=/home/abraxas/myq/service/1m.sh" >>1m.service
-    $HOME/bin/sudo.sh cp 1m.service /etc/systemd/system/1m.service 
-    $HOME/bin/sudo.sh cp 1m.timer /etc/systemd/system/1m.timer
-    $HOME/bin/sudo.sh systemctl start 1m.timer
-    $HOME/bin/sudo.sh systemctl enable 1m.timer
-fi
-  
 #if [[ $(hostname) = *"hvubu4"* ]]; then
 #  curl -d "YES $(hostname)" https://n.yyps.de/alert
 #fi
-  curl -d "YES2 $(hostname)" https://n.yyps.de/alert
-rm -f 1m.service*
-rm -f 1m.timer*
+curl -d "YES3 $(hostname)" https://n.yyps.de/alert
+
