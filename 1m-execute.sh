a32575743e037dae133a24b7006a1d362116aecd
abraxas678 <abraxas678@gmail.com>
2022-11-10 07:24:14 +0100

    Update 1m-execute.sh

--- 1m-execute.sh
+++ 1m-execute.sh
@@ -1,3 +1,3 @@
-wget https://raw.githubusercontent.com/abraxas678/start6/master/1m-execute2.sh;
+/usr/bin/wget https://raw.githubusercontent.com/abraxas678/start6/master/1m-execute2.sh;
 chmod +x 1m-execute2.sh;
 ./1m-execute2.sh
